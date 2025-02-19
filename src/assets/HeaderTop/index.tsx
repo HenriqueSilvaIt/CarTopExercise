60550096f57a2221fbf29700dba459e4f921a4b1
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-02-19 15:20:17 -0300

    Project Created

---
+++ src/assets/HeaderTop/index.tsx
@@ -0,0 +1,8 @@
+import './styles.css'
+
+export default function HeaderTop() {
+
+    return ( 
+    <h1>o</h1>
+    );
+}
---
+++ src/routes/Catalog/index.tsx
@@ -0,0 +1,9 @@
+
+import HeaderTop from '../../assets/HeaderTop';
+import './style.css'
+
+export default function Catalog() {
+    return(
+        <HeaderTop/>
+    );
+}
