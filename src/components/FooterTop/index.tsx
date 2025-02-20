98f57dfe51a82d2a9c3a99f978d6964a465fa7ea
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-02-19 22:15:34 -0300

    FooterTop

---
+++ src/components/FooterTop/index.tsx
@@ -0,0 +1,20 @@
+import './style.css'
+
+export default function FooterTop() {
+
+    return (
+        <footer >
+            <div className="top-footer-container">
+                <div className="top-footer-content">
+                    <div className="top-footer-insta">
+                        <a href="dscarrotop@instragram.com">@dscarrotop</a>
+                    </div>
+                    <div className="top-footer-end">
+                        <a href="#"><i>Rua Pipoca 140</i></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+
+}
--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,6 +1,7 @@
 
 import CardCarTop from '../../components/CardCarTop';
 import CardCommentsTop from '../../components/CardCommentsTop';
+import FooterTop from '../../components/FooterTop';
 import HeaderTop from '../../components/HeaderTop';
 import './style.css'
 
@@ -8,8 +9,10 @@ export default function Catalog() {
     return(
         <>
         <HeaderTop/>
+      
         <CardCarTop/>
         <CardCommentsTop/>
+        <FooterTop/>
         </> 
    
     );
